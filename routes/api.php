d809a62c317e160a75ae402a43676a3cdb3e10ef
user <user@gimminseong-ui-MacBookPro.local>
2021-02-12 20:43:37 +0900

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products', ProductController::class);
+
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews', ReviewController::class);
 });
