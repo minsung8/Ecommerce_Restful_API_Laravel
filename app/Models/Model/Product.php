dd6361404a280e5dd26e3c5a02aba8279aa7d694
user <user@gimminseong-ui-MacBookPro.local>
2021-02-13 14:41:47 +0900

    create relationship

--- app/Models/Model/Product.php
+++ app/Models/Model/Product.php
@@ -5,7 +5,13 @@ namespace App\Models\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Model\Review;
+
 class Product extends Model
 {
     use HasFactory;
+
+    public function reviews () {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Model/Review.php
+++ app/Models/Model/Review.php
@@ -5,7 +5,13 @@ namespace App\Models\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Model\Product;
+
 class Review extends Model
 {
     use HasFactory;
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
